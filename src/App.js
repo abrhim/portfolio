ca6894b3a10def5ab83d23df08938b9e9fb1a513
Abram Himmer <abram@Abrams-MacBook-Pro.local>
2019-01-03 17:13:15 -1000

    edited home page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+
+          This is my portfolio website. Behold, I am Abram. The Software Engineer.
           <a
             className="App-link"
             href="https://reactjs.org"
